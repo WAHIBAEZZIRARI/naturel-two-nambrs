eff4d35a51dc19add3d58bc0b18b02ad7d20e0d0
WAHIBAEZZIRARI <146207125+WAHIBAEZZIRARI@users.noreply.github.com>
2023-10-08 23:02:57 +0100

    Add files via upload

---
+++ naturel two nambrs.py
@@ -0,0 +1,6 @@
+A = int (input("Entrer la valeur de A: "))
+B = int (input("Entrer la valeur de B: "))
+if A*B>0 :
+   print("A et B ont le même signe ")
+else :
+    print("A et B ont deux signes différents ")
